5761635a4a365b28079b7b8976fa3cfcc28ec3e9
Romy Mae <romymaemilt@gmail.com>
2017-01-23 09:54:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/romymae/hackerman/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.romymae.hackerman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
